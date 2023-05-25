896cd57e81a5d934062312c6c37d94dbe17c987f
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-05-25 10:21:55 +0100

    javascript code added

---
+++ script.js
@@ -0,0 +1,55 @@
+// for smaller screen
+var sidemenu = document.getElementById("sidemenu");
+
+function barmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+// skill section
+const skillItems = document.querySelectorAll("section.skills .skill");
+
+skillItems.forEach(skill => {
+    skill.querySelector(".head").addEventListener("click", () => {
+        skill.querySelector(".items").classList.toggle("show-items");
+    })
+})
+
+// skill, education and experience button
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// contact form
+const scriptURL = 'https://script.google.com/macros/s/AKfycby2nt2AuBNvW6MzEwjPS25482jIhnkTfZFskmC813N-7g1ARZhG97SdmYjW46APlMPE/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 1000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
